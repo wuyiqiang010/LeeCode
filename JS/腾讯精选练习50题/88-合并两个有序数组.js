0cbbac0347b5d4edb6fb7b3999235fb9e5158097
wuyiqiang <wu_yiqiang@outlook.com>
2020-11-16 19:53:41 +0800

    "2020-11-16"

---
+++ JS/腾讯精选练习50题/88-合并两个有序数组.js
@@ -0,0 +1,40 @@
+/*
+* 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+说明：
+
+初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。
+你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+ 
+
+示例：
+
+输入：
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+输出：[1,2,2,3,5,6]
+ 
+
+提示：
+-10^9 <= nums1[i], nums2[i] <= 10^9
+nums1.length == m + n
+nums2.length == n
+* 
+* */
+
+
+function merge(nums1,m,nums2.n) {
+
+
+    var newarr=new Array()
+    var i=0,j=0
+    while(i<=m-1,j<=n-1){
+        if(nums1[i]<nums2[j]){
+            //添加到新数组中
+            newarr.push(nums1[i])
+            i++;
+        }else{
+            newarr.push[nums2[j]]
+            j++
+        }
+
+}
