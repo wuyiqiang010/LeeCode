317830c459e9ea4a42ad3714260453547074b95b
wu-yiqiang <wu_yiqiang@outlook.com>
2020-11-30 20:25:56 +0800

    链表反转

--- JavaScript/腾讯精选练习50题/21-合并两个有序链表.js
+++ JavaScript/腾讯精选练习50题/21-合并两个有序链表.js
@@ -9,5 +9,34 @@
 
 * */
 function mergeTwoLists(l1,l2) {
-    
+    var l1ptr=l1;
+    if(l1ptr==null){
+        return l2
+    }else{
+        while(l1ptr.next!=null){
+            l1ptr=l1ptr.next
+        }
+        l1ptr.next=l2
+        l1ptr=l1
+    }
+    //先将来l2链表合并到l1链表中，再对链表进行排序
+
+    //对链表进行排序
+    var La=l1ptr
+    while(La!=null){
+        var Lb=l1ptr
+        while(Lb!=null){
+            if (Lb.val>La.val){
+                let tmp
+                tmp=Lb.val
+                Lb.val=La.val
+                La.val=tmp
+            }
+            Lb=Lb.next
+
+        }
+        La=La.next
+    }
+    return l1
+
 }
