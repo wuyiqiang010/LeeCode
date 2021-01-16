f7462acc6cc3b996c107574df3b4f770f9c5cfba
YiQiangWu <wu_yiqiang@outlook.com>
2021-01-16 11:51:21 +0800

    调整目录结构

---
+++ Algorithms/QuickSort/Javascript.js
@@ -0,0 +1,19 @@
+/*
+* function:插入排序
+* */
+
+var arr=[12,34,1,3,5,6,7,3,4,56,34,0,0,1,2,223,3]
+function InsertionSort(arr,flags) {
+		//array表示需要进行排序的数组，flags表示控制位，true表示降序排列，false表示升序排列
+	for (let i=1;i<arr.length;i++){
+		let startpoint=i
+		while (startpoint<i){
+
+
+			startpoint--
+		}
+	}
+	return arr
+}
+
+console.log(InsertionSort(arr,true))
